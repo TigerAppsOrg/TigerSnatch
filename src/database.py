ebd45eef769787f985c6f358e0aca59f7863b38b
Nicholas Padmanabhan <nicholaspad@gmail.com>
2021-08-15 10:11:24 -0700

    Fix text in status indicator

--- src/database.py
+++ src/database.py
@@ -281,12 +281,12 @@ class Database:
             "notifs_schedule"
         ]
         if len(curr) == 0:
-            return ""
+            return "Next notifications period is not scheduled. Notify a TigerApps member if this isn't fixed soon!"
         start, end = tz_utc.localize(curr[0][0]), tz_utc.localize(curr[0][1])
         start, end = start.astimezone(tz_et), end.astimezone(tz_et)
         now = datetime.now(tz_et)
         if now >= end:
-            return ""
+            return "Next notifications period is not scheduled. Notify a TigerApps member if this isn't fixed soon!"
         end_fmt = end.strftime(fmt)
         if now >= start:
             return f"Current notifications period will end on {end_fmt} ET."
