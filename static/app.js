f595c1cd35ff31c21fae0a072735232b7d37cf1e
Nicholas Padmanabhan <nicholaspad@gmail.com>
2021-08-19 15:02:07 -0700

    Add detailed disclaimer on Trades modal
    "Clicking 'Email' will add an entry for you and your match in Activity. Exchanging seats is your and your match's responsibilities. Instructors are not responsible if your seats are taken by other students during the exchange."

--- static/app.js
+++ static/app.js
@@ -1334,7 +1334,7 @@ let findMatches = function () {
                             <path
                                 d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
                             />
-                        </svg> Clicking 'Email' will add an entry for you & the match in Activity.
+                        </svg> Clicking 'Email' will add an entry for you and your match in Activity. Exchanging seats is your and your match's responsibilities. Instructors are not responsible if your seats are taken by other students during the exchange.
                     </div>
                     <div class="table-responsive">
                         <table class="table table-hover mt-2">
