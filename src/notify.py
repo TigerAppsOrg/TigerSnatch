6502a39746317cce11d4821db89de99336867f38
Nicholas Padmanabhan <nicholaspad@gmail.com>
2021-08-23 22:11:26 -0400

    Group section emails together in single SendGrid API call to increase efficiency

--- src/notify.py
+++ src/notify.py
@@ -14,38 +14,40 @@ class Notify:
     # to format and send an email to the first student on the waitlist
     # for that classid
 
-    def __init__(self, classid, i, n_new_slots, db):
+    def __init__(self, classid, n_new_slots, db):
         self._classid = classid
         try:
             self._deptnum, self._title, self._sectionname = db.classid_to_classinfo(
                 classid
             )
             self._coursename = f"{self._deptnum}: {self._title}"
-            self._netid = db.get_class_waitlist(classid)["waitlist"][i]
+            self._netids = db.get_class_waitlist(classid)["waitlist"]
+
+            user_log = (
+                f"{n_new_slots} spots available in {self._deptnum} {self._sectionname}"
+            )
+            self._emails = []
+            for netid in self._netids:
+                self._emails.append(db.get_user(netid, "email"))
+                db.update_user_waitlist_log(netid, user_log)
         except:
             raise Exception(
-                f"waitlist element {i} for class {classid} does not exist; user probably removed themself"
+                f"unable to get notification data for subscriptions of class {classid}"
             )
-        self._email = db.get_user(self._netid, "email")
 
-        user_log = (
-            f"{n_new_slots} spots available in {self._deptnum} {self._sectionname}"
-        )
-        db.update_user_waitlist_log(self._netid, user_log)
+    # returns the netIDs of this Notify object
 
-    # returns the primary netid of this Notify object
-
-    def get_netid(self):
-        return self._netid
+    def get_netids(self):
+        return self._netids
 
     # sends a formatted email
 
-    def send_email_html(self):
+    def send_emails_html(self):
         msg = f"""\
         <html>
         <head></head>
         <body style='font-size:1.3em'>
-            <p>Dear {self._netid},</p>
+            <p>Greetings $$netid$$,</p>
             <p>Your subscribed section <b>{self._sectionname}</b> in <b>{self._coursename}</b> has one or more spots open!</p>
             <p>Head over to <a href="https://phubprod.princeton.edu/psp/phubprod/?cmd=start">TigerHub</a> to Snatch your spot!</p>
             <p>You'll continue to receive notifications for this section every 2 minutes if spots are still available. To unsubscribe from notifications for this section, visit your <a href="https://snatch.tigerapps.org/dashboard">TigerSnatch Dashboard</a>.</p>
@@ -53,16 +55,21 @@ class Notify:
         </body>
         </html>"""
 
-        message = Mail(
-            from_email=TS_EMAIL,
-            to_emails=self._email,
-            subject=f"TigerSnatch: a spot opened in {self._deptnum} {self._sectionname}",
-            html_content=msg,
-        )
+        data = {
+            "personalizations": [
+                {
+                    "to": [{"email": self._emails[i]}],
+                    "subject": f"TigerSnatch: a spot opened in {self._deptnum} {self._sectionname}",
+                    "substitutions": {"$$netid$$": self._netids[i]},
+                }
+                for i in range(len(self._emails))
+            ],
+            "from": {"email": TS_EMAIL},
+            "content": [{"type": "text/html", "value": msg}],
+        }
 
         try:
-            sg = SendGridAPIClient(SENDGRID_API_KEY)
-            sg.send(message)
+            SendGridAPIClient(SENDGRID_API_KEY).client.mail.send.post(request_body=data)
             return True
         except Exception as e:
             print(e, file=stderr)
@@ -70,8 +77,8 @@ class Notify:
 
     def __str__(self):
         ret = "Notify:\n"
-        ret += f"\tNetID:\t\t{self._netid}\n"
-        ret += f"\tEmail:\t\t{self._email}\n"
+        ret += f"\tNetIDs:\t\t{self._netids}\n"
+        ret += f"\tEmails:\t\t{self._emails}\n"
         ret += f"\tCourse:\t\t{self._coursename}\n"
         ret += f"\tSection:\t{self._sectionname}\n"
         ret += f"\tClassID:\t{self._classid}"
