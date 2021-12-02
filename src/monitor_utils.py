9bc2d8682e47567fe8aeadaa104ba305e7a1652d
Nicholas Padmanabhan <nicholaspad@gmail.com>
2021-12-02 10:12:09 -0500

    Clean course update code

--- src/monitor_utils.py
+++ src/monitor_utils.py
@@ -9,8 +9,6 @@ from mobileapp import MobileApp
 from coursewrapper import CourseWrapper
 from sys import stderr
 
-# _api = MobileApp()
-
 
 # gets the latest term code
 def get_latest_term():
@@ -49,10 +47,10 @@ def get_new_mobileapp_data(term, course, classes, default_empty_dicts=False):
 # returns course data and parses its data into dictionaries
 # ready to be inserted into database collections
 def get_course_in_mobileapp(term, course_, curr_time, db: Database):
-    _api = MobileApp()
-
     # the prepended space in catnum is intentional
-    data = _api.get_courses(term=term, subject=course_[:3], catnum=f" {course_[3:]}")
+    data = MobileApp().get_courses(
+        term=term, subject=course_[:3], catnum=f" {course_[3:]}"
+    )
 
     if "subjects" not in data["term"][0]:
         raise RuntimeError("no query results")
