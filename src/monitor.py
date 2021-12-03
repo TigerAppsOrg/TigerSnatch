e7dff8d1b50210094dc084b588d5d5e9eb8f9bd1
Nicholas Padmanabhan <nicholaspad@gmail.com>
2021-12-03 10:38:32 -0500

    Revert "Get usable CPU count rather than CPU count of system"
    This reverts commit 95c73564b40380e6013acb0f4bb8f5f79c976162.

--- src/monitor.py
+++ src/monitor.py
@@ -6,7 +6,7 @@
 
 from database import Database
 from multiprocess import Pool
-from os import sched_getaffinity
+from os import cpu_count
 from time import time
 from sys import stderr
 from monitor_utils import get_latest_term, process, get_course_in_mobileapp
@@ -51,7 +51,7 @@ class Monitor:
             process_args.append([term, course, classes[1:], classes[0]])
 
         # alleviate MobileApp bottleneck using multiprocessing
-        with Pool(len(sched_getaffinity(0))) as pool:
+        with Pool(cpu_count()) as pool:
             all_data = pool.map(process, process_args)
 
         self._waited_course_wrappers = all_data
