57ba0dce225fe4635e50f80541922ecd7b7ee1c3
Nicholas Padmanabhan <nicholaspad@gmail.com>
2021-12-23 16:36:21 -0800

    Clean mobileapp code and requirements

--- src/mobileapp.py
+++ src/mobileapp.py
@@ -8,7 +8,6 @@
 import requests
 import json
 import base64
-from bs4 import BeautifulSoup
 from config import CONSUMER_KEY, CONSUMER_SECRET
 from database import Database
 from time import time
@@ -33,33 +32,6 @@ class MobileApp:
 
     def get_terms(self):
         return self._getJSON(self.configs.COURSE_TERMS, fmt="json")
-
-    def get_course_from_registrar_api(self, **kwargs):
-        tic = time()
-        req = requests.get(
-            self.configs.REGISTRAR_API_URL,
-            params=kwargs if "kwargs" not in kwargs else kwargs["kwargs"],
-            headers={"Authorization": "Bearer " + self.configs.REGISTRAR_ACCESS_TOKEN},
-        )
-        if req.status_code != 200:
-            self.configs._refreshRegistrarToken()
-            req = requests.get(
-                self.configs.REGISTRAR_API_URL,
-                params=kwargs if "kwargs" not in kwargs else kwargs["kwargs"],
-                headers={
-                    "Authorization": "Bearer " + self.configs.REGISTRAR_ACCESS_TOKEN
-                },
-            )
-        self._db._add_system_log(
-            "registrar",
-            {
-                "message": "Registrar API query",
-                "response_time": time() - tic,
-                "endpoint": self.configs.REGISTRAR_API_URL,
-                "args": kwargs,
-            },
-        )
-        return json.loads(req.text)
 
     """
     This function allows a user to make a request to 
@@ -117,15 +89,8 @@ class Configs:
         self.BASE_URL = "https://api.princeton.edu:443/mobile-app"
         self.COURSE_COURSES = "/courses/courses"
         self.COURSE_TERMS = "/courses/terms"
-        self.REGISTRAR_API_URL = (
-            "https://api.princeton.edu/registrar/course-offerings/course-details"
-        )
         self.REFRESH_TOKEN_URL = "https://api.princeton.edu:443/token"
-        self.REGISTRAR_REFRESH_TOKEN_URL = (
-            "https://registrar.princeton.edu/course-offerings"
-        )
         self._refreshToken(grant_type="client_credentials")
-        self._refreshRegistrarToken()
 
     def _refreshToken(self, **kwargs):
         req = requests.post(
@@ -142,19 +107,8 @@ class Configs:
         response = json.loads(text)
         self.ACCESS_TOKEN = response["access_token"]
 
-    def _refreshRegistrarToken(self):
-        req = requests.get(self.REGISTRAR_REFRESH_TOKEN_URL)
-        soup = BeautifulSoup(req.text, features="lxml")
-        data = soup.find(
-            "script", {"data-drupal-selector": "drupal-settings-json"}, text=False
-        )
-        self.REGISTRAR_ACCESS_TOKEN = json.loads(data.contents[0])["ps_registrar"][
-            "apiToken"
-        ]
-
 
 if __name__ == "__main__":
     api = MobileApp()
     # print(api.get_courses(term='1214', subject='list'))
     # print(api.get_courses(term="1214", search="NEU350"))
-    print(api.get_course_from_registrar_api(term="1224", course_id="014003"))
