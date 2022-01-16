792cead9939811e431c0a96168841addd17b67e6
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-01-15 21:43:43 -0800

    Implement suggestions from @shannon-heh
    - --test flag now takes specific email addresses

--- src/_email_all_users.py
+++ src/_email_all_users.py
@@ -7,20 +7,21 @@
 # Script to email all TigerSnatch users. This uses a SendGrid dynamic
 # template (edit on SendGrid --> Dynamic Templates). To use this script:
 #
-# 1. Create a file named MESSAGE in the same directory as this script
-#    and type the body of your email (can include new-lines and HTML
-#    tags).
-# 2. Create a file named SUBJECT in the same directory as this script
-#    and type the subject of your email (must be on a single line,
-#    without any HTML tags).
+# 1. Create a file named MESSAGE (no extension) in the same directory as
+#    this script and type the body of your email (can include new-lines
+#    and HTML tags).
+# 2. Create a file named SUBJECT (no extension) in the same directory as
+#    this script and type the subject of your email (must be on a single
+#    line, without any HTML tags).
 # 3. Execute the script. Always test your email by first sending it to
 #    admins with --test, before sending it to everyone with --all!
 #
 #    Specify one of the following flags:
-#       --test: Send email to only admins
-# 	    --all: Send email to all users
+#       --test <email1> <email2> ... : Send to 1+ specific emails.
+# 	    --all: Send to ALL user emails.
 #
-#    Example: python _email_all_users.py --test
+#    Example: python _email_all_users.py --test x@x.com y@y.com
+#    Example: python _email_all_users.py --all
 # ----------------------------------------------------------------------
 
 from database import Database
@@ -31,10 +32,10 @@ from config import SENDGRID_API_KEY, TS_EMAIL
 if __name__ == "__main__":
 
     def process_args():
-        if len(argv) != 2 or (argv[1] != "--test" and argv[1] != "--all"):
+        if len(argv) < 2 or (argv[1] != "--test" and argv[1] != "--all"):
             print("specify one of the following flags:")
-            print("\t--test: send email to only admins")
-            print("\t--all: send email to ALL users")
+            print("\t--test <email1> <email2> ...: send to 1+ specific emails")
+            print("\t--all: send to ALL user emails")
             exit(2)
         return argv[1] == "--all"
 
@@ -42,23 +43,33 @@ if __name__ == "__main__":
     SUBJECT_FILEPATH = f"{DIR}/SUBJECT"
     MESSAGE_FILEPATH = f"{DIR}/MESSAGE"
 
-    with open(SUBJECT_FILEPATH, "r") as subj, open(MESSAGE_FILEPATH, "r") as msg:
-        SUBJECT = subj.read()
-        MESSAGE = msg.read()
+    try:
+        with open(SUBJECT_FILEPATH, "r") as subj, open(MESSAGE_FILEPATH, "r") as msg:
+            SUBJECT = subj.read()
+            MESSAGE = msg.read()
+    except FileNotFoundError:
+        print("make sure that files SUBJECT and MESSAGE (no extensions) exist in /src!")
+        exit(1)
 
     send_to_all_users = process_args()
-    send_to_admins = not send_to_all_users
-    db = Database()
-    emails = (
-        db._get_all_emails_csv() if send_to_all_users else db._get_admin_emails_csv()
-    ).split(",")
+    send_to_custom = not send_to_all_users
+
+    if send_to_custom and len(argv) < 3:
+        print("--test requires a space-separated list of 1+ email addresses!")
+        print("Example: $ python _email_all_users.py --test x@x.com y@y.com")
+        exit(1)
+
+    if send_to_all_users:
+        emails = Database()._get_all_emails_csv().split(",")
+    else:
+        emails = argv[2:]
 
     SUBJECT = SUBJECT.strip()
     MESSAGE = MESSAGE.strip().replace("\n", "<br>")
 
-    if send_to_admins:
+    if send_to_custom:
         SUBJECT = f"[Test] {SUBJECT}"
-        MESSAGE = f"[This is a test email sent to TigerSnatch admins only]<br>{MESSAGE}"
+        MESSAGE = f"[This is a test email]<br>{MESSAGE}"
 
     print("---------------")
     print("SUBJECT:", SUBJECT)
@@ -69,7 +80,7 @@ if __name__ == "__main__":
     print()
     if (
         input(
-            f"Send the above email to {'ALL users' if send_to_all_users else 'admins only'} ({len(emails)} in total)? (y/n) "
+            f"Send the above email to {'ALL users' if send_to_all_users else argv[2:]} ({len(emails)} in total)? (y/n) "
         )
         != "y"
     ):
